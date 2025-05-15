d01faf5d9767c67ceb42b35a67677bfa10aeef66
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-05-15 13:21:08 +0900

    firstCommit

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask
+from flask import render_template,g,redirect,request,flash,url_for
+import sqlite3
+DATABASE = "flasktodo.db"
+
+app = Flask(__name__)
+app.secret_key = 'dev_secret_key'
+
+@app.route("/")
+def main():
+  todo_list = get_db().execute("SELECT id,title,detail,deadline,status FROM todo WHERE status = '進行中' ORDER BY deadline ASC").fetchall()
+  return render_template("index.html",todo_list = todo_list)
+
+@app.route("/todo_all")
+def all():
+  todo_list = get_db().execute("SELECT id,title,detail,deadline,status FROM todo ORDER BY deadline ASC").fetchall()
+  check = any(todo['status'] == '完了' for todo in todo_list)
+  return render_template("index.html",todo_list = todo_list,check=check)
+
+@app.route("/todo_regist", methods=['GET', 'POST'])
+def regist():
+  if request.method == "POST":
+    # 画面からの登録情報の取得
+    title = request.form.get('title')
+    detail = request.form.get('detail')
+    deadline = request.form.get('deadline')
+    status = request.form.get('status')
+    if title == "":
+      flash("件名は必須です")
+      return render_template("/todo_regist.html",title=title,detail=detail,deadline=deadline,status=status)
+    if status == "":
+      flash("状況が選択されていません")
+      return render_template("/todo_regist.html",title=title,detail=detail,deadline=deadline,status=status)
+    db = get_db()
+    db.execute("INSERT INTO todo (title,detail,deadline,status) VALUES (?,?,?,?)", [title,detail,deadline,status])
+    db.commit()
+    return redirect("/")
+  return render_template("todo_regist.html")
+
+@app.route("/<id>/todo_edit", methods=["GET", "POST"])
+def edit(id):
+  if request.method == "POST":
+    #画面からの登録情報の取得
+    title = request.form.get("title")
+    detail = request.form.get("detail")
+    deadline = request.form.get("deadline")
+    status = request.form.get("status")
+    if title == "":
+      flash("件名は必須です")
+      return render_template("/todo_regist.html",title=title,detail=detail,deadline=deadline,status=status)
+    if status == "":
+      flash("状況が選択されていません")
+      return render_template("/todo_regist.html",title=title,detail=detail,deadline=deadline,status=status)
+    db = get_db()
+    db.execute("UPDATE todo SET title=?, detail=?, deadline=?, status=? WHERE id =?",[title,detail,deadline,status,id])
+    db.commit()
+    return redirect("/")
+  post = get_db().execute("SELECT id,title,detail,deadline,status FROM todo WHERE id=?",(id,)).fetchone()
+  return render_template("todo_edit.html",post=post)
+
+@app.route("/todo_status/<id>", methods=["POST"])
+def update_status(id):
+  status = request.json.get("status")
+  db = get_db()
+  db.execute("UPDATE todo SET status = ? WHERE id = ?", (status, id))
+  db.commit()
+  return "", 204
+
+@app.route("/<id>/todo_delete", methods=["GET"])
+def delete(id):
+    db = get_db()
+    db.execute("DELETE FROM todo WHERE id =?",(id))
+    db.commit()
+    return redirect("/")
+
+@app.errorhandler(404)
+def not_found_err(error):
+  return render_template('404.html'),404
+
+@app.errorhandler(500)
+def not_found_err(error):
+  return render_template('500.html'),500
+
+if __name__ == "__main__":
+  app.run()
+
+# DATABASE
+def connect_db():
+  rv = sqlite3.connect(DATABASE)
+  rv.row_factory = sqlite3.Row
+  return rv
+def get_db():
+  if not hasattr(g,'sqlite.db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
---
+++ static/js/main.js
@@ -0,0 +1,131 @@
+document.querySelectorAll(".status-select").forEach(select => {
+  select.addEventListener("change", function () {
+    const id = this.dataset.id;
+    const status = this.value;
+    updateStatus(id, status)
+  })
+});
+
+function updateStatus(id, newStatus) {
+  fetch(`/todo_status/${id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ status: newStatus })
+  })
+  .then(response => {
+    if (response.ok) {
+      if (newStatus === '完了') {
+        const row = document.getElementById(`todo-${id}`);
+        if (row) row.remove();  // 表示から削除
+      }
+    } else {
+      console.error('ステータス更新失敗');
+    }
+  })
+  .catch(error => {
+    console.error('通信エラー:', error);
+  });
+}
+
+const path = anime.path('#motionPath');
+let flipped = false;
+
+const swing = {
+  targets: ".chara01 img", // 左右に揺らす動きを新規作成したimgタグに設定する
+  easing: "linear", // イージングは一定（linear）に設定
+  rotate: [0, -15], // 左右に10度ずつ揺らす
+  duration: 1000, // 歩く速度の1/10くらいに設定
+  loop: true, // 揺れを繰り返す
+};
+
+const swing2 = {
+  targets: ".chara02 img", // 左右に揺らす動きを新規作成したimgタグに設定する
+  easing: "linear", // イージングは一定（linear）に設定
+  delay: 300,
+  duration: 10000,
+  scale: [0.5, 1, 1,5, 2, 3],
+  rotate: [10, -10], // 左右に10度ずつ揺らす
+  direction: "alternate", // 揺れる方向を設定。「行って戻る」ように揺らがせる
+  loop: true, // 揺れを繰り返す
+};
+
+function startWalk() {
+  anime({
+    targets: ".chara03",
+    translateX: 1500,
+    scaleX: 1,
+    easing: "easeInQuad",
+    duration: 4000,
+    delay: 1000,
+    complete: function() {
+      flipped = true;
+      anime({
+        targets: ".chara03",
+        scaleX: -1, // 反転してから
+        duration: 3000, 
+        complete: function() {
+          anime({
+            targets: ".chara03",
+            translateX: 0,
+            easing: "easeOutQuad",
+            duration: 3000,
+            complete: function() {
+              flipped = false;
+              anime({
+                targets: ".chara03",
+                scaleX: 1,
+                duration: 0,
+                complete: startWalk // 再帰でループ
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+function startWalk2() {
+  anime({
+    targets: ".chara01",
+    translateX: -1050,
+    scaleX: 1,
+    easing: "linear",
+    duration: 4000,
+    delay: 1000,
+    complete: function() {
+      flipped = true;
+      anime({
+        targets: ".chara01",
+        scaleX: -1, // 反転してから
+        duration: 0, // 一瞬で
+        complete: function() {
+          anime({
+            targets: ".chara01",
+            translateX: 0,
+            easing: "linear",
+            duration: 4000,
+            delay: 1000,
+            complete: function() {
+              flipped = false;
+              anime({
+                targets: ".chara01",
+                scaleX: 1,
+                duration: 0,
+                complete: startWalk2 // 再帰でループ
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+
+// anime(walk)
+startWalk();
+startWalk2();
+anime(swing)
+anime(swing2)
